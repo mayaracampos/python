1297a1e860e03e0c34cabd232ac2e29bf09f253f
Mayara Campos <mayaracampos@Mayaras-MacBook-Pro.local>
2015-09-14 14:00:59 -0300

    repositório

---
+++ src/profiles/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+	name = models.CharField(max_length = 1200)
+	description = models.TextField(default='description default')
+
+def __unicode__(self):
+		return self.name
